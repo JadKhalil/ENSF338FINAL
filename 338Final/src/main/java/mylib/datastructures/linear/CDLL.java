cd69b2c33fa7c757c6f52b3ea579281d84f24868
JadKhalil <113618569+JadKhalil@users.noreply.github.com>
2023-04-02 16:29:50 -0700

    Delete 338Final directory

--- 338Final/src/main/java/mylib/datastructures/linear/CDLL.java
+++
@@ -1,7 +0,0 @@
-package main.java.mylib.datastructures.linear;
-
-import main.java.mylib.datastructures.nodes.DNode;
-
-public class CDLL extends DLL {
-
-}
--- 338Final/src/main/java/mylib/datastructures/trees/AVL.java
+++
@@ -1,5 +0,0 @@
-package main.java.mylib.datastructures.trees;
-
-public class AVL extends BST {
-
-}
