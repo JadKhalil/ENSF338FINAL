e0226e90a7e759148d885a694464c79a2bb47448
JeanRou <jeannicolas.rouette@ucalgary.ca>
2023-04-06 19:16:03 -0600

    Extensive test for AVL

--- src/main/java/mylib/App.java
+++ src/main/java/mylib/App.java
@@ -22,35 +22,18 @@ class App {
 
         System.out.println("before");
         avl.printBF();
+        System.out.println("b:");
+        System.out.println(Math.abs(avl.getRoot().getBalance()));
 
         avl.Delete(10);
         avl.Delete(11);
 
-        System.out.println(":After");
+        System.out.println("after");
         avl.printBF();
+        System.out.println("b:");
+        System.out.println(Math.abs(avl.getRoot().getBalance()));
 
-        avl.Delete(0);
-        avl.printBF();
 
-        // System.out.println(avl.getRoot().getLeft().getParent().getData());
-
-        // BST x = new BST();
-        // x.Insert(9);
-        // x.Insert(5);
-        // x.Insert(10);
-        // x.Insert(0);
-        // x.Insert(6);
-        // x.Insert(11);
-        // x.Insert(negativeNum);
-        // x.Insert(1);
-        // x.Insert(2);
-
-        // // x.Delete(10);
-        // x.Delete(0);
-        // x.Delete(9);
-        // x.printBF();
-
-        // System.out.println(x.getRoot().getRight().getParent().getData());
 
     }
 }
--- src/test/java/mylib/datastructures/trees/AVLTest.java
+++ src/test/java/mylib/datastructures/trees/AVLTest.java
@@ -157,7 +157,6 @@ public class AVLTest {
         Assert.assertEquals(70, avl.getRoot().getRight().getData());
         Assert.assertEquals(60, avl.getRoot().getRight().getLeft().getData());
         Assert.assertEquals(80, avl.getRoot().getRight().getRight().getData());
-
     }
 
     @Test
@@ -177,23 +176,45 @@ public class AVLTest {
     @Test
     public void testMultipleDeletionsMaintainBalance() {
         AVL avl = new AVL();
-        int negativeNum = -1;
         avl.Insert(9);
         avl.Insert(5);
         avl.Insert(10);
         avl.Insert(0);
         avl.Insert(6);
         avl.Insert(11);
-        avl.Insert(negativeNum);
+        avl.Insert(4);
         avl.Insert(1);
         avl.Insert(2);
 
         avl.Delete(10);
         avl.Delete(11);
 
-        Assert.assertEquals(1, avl.getRoot().getData());
-        Assert.assertEquals(0, avl.getRoot().getLeft().getData());
-        Assert.assertEquals(5, avl.getRoot().getRight().getData());
+        Assert.assertTrue(Math.abs(avl.getRoot().getBalance()) <= 1);
+
+        Assert.assertEquals(4, avl.getRoot().getData());
+        Assert.assertEquals(1, avl.getRoot().getLeft().getData());
+        Assert.assertEquals(6, avl.getRoot().getRight().getData());
+        Assert.assertEquals(0, avl.getRoot().getLeft().getLeft().getData());
+        Assert.assertEquals(2, avl.getRoot().getLeft().getRight().getData());
+        Assert.assertEquals(5, avl.getRoot().getRight().getLeft().getData());
+        Assert.assertEquals(9, avl.getRoot().getRight().getRight().getData());
+
+        Assert.assertEquals(4, avl.getRoot().getLeft().getParent().getData());
+        Assert.assertEquals(4, avl.getRoot().getRight().getParent().getData());
+        Assert.assertEquals(1, avl.getRoot().getLeft().getLeft().getParent().getData());
+        Assert.assertEquals(1, avl.getRoot().getLeft().getRight().getParent().getData());
+        Assert.assertEquals(6, avl.getRoot().getRight().getLeft().getParent().getData());
+        Assert.assertEquals(6, avl.getRoot().getRight().getRight().getParent().getData());
+
+        Assert.assertNull(avl.getRoot().getParent());
+        Assert.assertNull(avl.getRoot().getLeft().getLeft().getLeft());
+        Assert.assertNull(avl.getRoot().getLeft().getLeft().getRight());
+        Assert.assertNull(avl.getRoot().getLeft().getRight().getLeft());
+        Assert.assertNull(avl.getRoot().getLeft().getRight().getRight());
+        Assert.assertNull(avl.getRoot().getRight().getLeft().getLeft());
+        Assert.assertNull(avl.getRoot().getRight().getLeft().getRight());
+        Assert.assertNull(avl.getRoot().getRight().getRight().getLeft());
+        Assert.assertNull(avl.getRoot().getRight().getRight().getRight());
     }
 
     @Test
