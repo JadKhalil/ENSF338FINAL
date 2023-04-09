2a553e278d83e85e6d20f7132f20c92a2656abfc
JadKhalil <jad.khalil1@ucalgary.ca>
2023-04-09 11:55:22 -0600

    final changes before library

--- src/main/java/mylib/App.java
+++ src/main/java/mylib/App.java
@@ -9,31 +9,30 @@ class App {
 
     public static void main(String args[]) {
         AVL avl = new AVL();
-        int negativeNum = -1;
         avl.Insert(9);
         avl.Insert(5);
         avl.Insert(10);
         avl.Insert(0);
         avl.Insert(6);
         avl.Insert(11);
-        avl.Insert(negativeNum);
+        avl.Insert(4);
         avl.Insert(1);
         avl.Insert(2);
 
-        System.out.println("before");
-        avl.printBF();
-        System.out.println("b:");
-        System.out.println(Math.abs(avl.getRoot().getBalance()));
-
         avl.Delete(10);
         avl.Delete(11);
-
-        System.out.println("after");
+        // System.out.println("before");
         avl.printBF();
-        System.out.println("b:");
-        System.out.println(Math.abs(avl.getRoot().getBalance()));
+        
+        // System.out.println(Math.abs(avl.getRoot().getBalance()));
 
+        // avl.Delete(10);
+        // avl.Delete(11);
 
+        // System.out.println("after");
+        // avl.printBF();
+        // System.out.println("b:");
+        // System.out.println(Math.abs(avl.getRoot().getBalance()));
 
     }
 }
